f2889888b037ca2e4b37eec326caef05a8e3a573
Nick Pisani <napisani@yahoo.com>
2024-02-17 18:03:00 -0500

    better sub

--- cmd/commandline/main.go
+++ cmd/commandline/main.go
@@ -9,16 +9,16 @@ import (
 	"golang.design/x/clipboard"
 )
 
-type JSONRecord = map[string]interface{}
-type Pipeline = []JSONRecord
+const LEFT_PARENTHESIS_TOKEN = "__LEFT_PARENTHESIS__"
+const RIGHT_PARENTHESIS_TOKEN = "__RIGHT_PARENTHESIS__"
 
 func adjustValue(value interface{}) interface{} {
 	adjustedValue := value
 	if value != nil && reflect.TypeOf(value).Kind() == reflect.String {
 		if regexp.MustCompile(`^[0-9a-fA-F]{24}$`).MatchString(value.(string)) {
-			adjustedValue = "ObjectId(\"" + value.(string) + "\")"
+			adjustedValue = "ObjectId" + LEFT_PARENTHESIS_TOKEN + value.(string) + RIGHT_PARENTHESIS_TOKEN
 		} else if regexp.MustCompile(`.*T[0-9][0-9]:[0-9][0-9]:[0-9][0-9].*Z$`).MatchString(value.(string)) {
-			adjustedValue = "ISODate(\"" + value.(string) + "\")"
+			adjustedValue = "ISODate" + LEFT_PARENTHESIS_TOKEN + value.(string) + RIGHT_PARENTHESIS_TOKEN
 		}
 	}
 	return adjustedValue
@@ -51,12 +51,12 @@ func traverseArray(anArray []interface{}) {
 }
 
 func doRegexReplacements(jsonText string) string {
-  result := jsonText
-  for _, r := range []string{"ObjectId", "ISODate"} {
-    result = regexp.MustCompile(`"`+ r + `\(\\`).ReplaceAllString(result, r + "(")
-  }
-  result = regexp.MustCompile(`\\"\)"`).ReplaceAllString(result, "\")")
-  return result
+	result := jsonText
+	for _, r := range []string{"ObjectId", "ISODate"} {
+    result = regexp.MustCompile(`"`+r+LEFT_PARENTHESIS_TOKEN).ReplaceAllString(result, r+"(\"")
+	}
+	result = regexp.MustCompile(RIGHT_PARENTHESIS_TOKEN+"\"").ReplaceAllString(result, "\")")
+	return result
 }
 
 func main() {
@@ -65,29 +65,29 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("Reading clipboard... ")
-  fmt.Println("")
+	fmt.Println("")
 	text := clipboard.Read(clipboard.FmtText)
 	var pipeline interface{}
 	err = json.Unmarshal(text, &pipeline)
 	if err != nil {
-    fmt.Println("Error: Invalid JSON in clipboard. Please copy a valid JSON representation of a MongoDB pipeline.")
+
 		panic(err)
 	}
-  fmt.Println("Converting...")
-  fmt.Println("")
+	fmt.Println("Converting...")
+	fmt.Println("")
 	traverseArray(pipeline.([]interface{}))
 	jsonBytes, err := json.MarshalIndent(pipeline, "", "  ")
 
-  result := string(jsonBytes)	
-  result = doRegexReplacements(result)
-  fmt.Println("Writing clipboard...")
-  fmt.Println("")
+	result := string(jsonBytes)
+	result = doRegexReplacements(result)
+	fmt.Println("Writing clipboard...")
+	fmt.Println("")
 	clipboard.Write(clipboard.FmtText, []byte(result))
-  fmt.Println("----------------------------")
-  fmt.Println("")
-  fmt.Println(result)
-  fmt.Println("")
-  fmt.Println("----------------------------")
-  fmt.Println("Done!")
+	fmt.Println("----------------------------")
+	fmt.Println("")
+	fmt.Println(result)
+	fmt.Println("")
+	fmt.Println("----------------------------")
+	fmt.Println("Done!")
 
 }
