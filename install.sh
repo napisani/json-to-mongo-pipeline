f7fba7fdb2219bb00d2548ff98f8e829d5f3f335
Nick Pisani <napisani@yahoo.com>
2024-03-13 21:19:22 -0400

    fix

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # change this tag when a new release is made
-TAG="v1.2"
+TAG="v1.3"
 IS_MACOS=$(uname -s | grep -i Darwin)
 IS_LINUX=$(uname -s | grep -i Linux)
 ARCH=$(uname -m)
@@ -9,7 +9,7 @@ echo "Detected OS: $IS_MACOS $IS_LINUX $ARCH"
 if [[ -n "$IS_MACOS" || -n "$IS_LINUX" ]]; then
 	echo "Downloading binary for tag $TAG"
 	OS="$([[ -n "$IS_MACOS" ]] && printf Darwin || printf Linux)"
-	URL="https://github.com/napisani/json-to-mongo-pipeline-go/releases/download/${TAG}/json-to-mongo-pipeline_${OS}_${ARCH}"
+	URL="https://github.com/napisani/json-to-mongo-pipeline-go/releases/download/${TAG}/json-to-mongo-pipeline-go_${OS}_${ARCH}"
   echo "Downloading from: $URL"
 	curl -L -o json-to-mongo-pipeline "$URL"
 	chmod +x json-to-mongo-pipeline
